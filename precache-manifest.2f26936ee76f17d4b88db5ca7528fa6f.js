067edc81b27b5db034ea1b1dc1e7c234e82ba671
Palash Tiwari <paltiw018@gmail.com>
2022-10-23 10:37:58 +0530

    Updates

---
+++ precache-manifest.2f26936ee76f17d4b88db5ca7528fa6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3feb76c477c81506550f",
+    "url": "/testproject/static/css/main.8eafc7a0.chunk.css"
+  },
+  {
+    "revision": "3feb76c477c81506550f",
+    "url": "/testproject/static/js/main.3feb76c4.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/testproject/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "4a2a22489d72857e3768",
+    "url": "/testproject/static/js/2.4a2a2248.chunk.js"
+  },
+  {
+    "revision": "e4070637eb5369e88069",
+    "url": "/testproject/static/js/runtime~main.e4070637.js"
+  },
+  {
+    "revision": "4335abe250842eb12bcbcde92aa77330",
+    "url": "/testproject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.737761b420af41738432761f4818f5d9.js"
+  "/testproject/precache-manifest.2f26936ee76f17d4b88db5ca7528fa6f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/testproject/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
